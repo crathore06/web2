156190b31911574820720157829d0485e2941e55
chetanrathore423@gmail.com <chetanrathore423@gmail.com>
2026-06-19 03:53:33 +0530

    Added new C++ programs

---
+++ c++/project2/A_basic_calculater/hello.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int x, y;
+    int sum;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "The sum of x and y is :" << sum;
+
+    return 0;
+
+}
