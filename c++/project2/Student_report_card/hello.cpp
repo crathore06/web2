549a5cb37d462bbdaf4d82aa34f4b08f33629c80
chetanrathore423@gmail.com <chetanrathore423@gmail.com>
2026-06-19 04:14:48 +0530

    Added new C++ programs

---
+++ c++/project2/Student_report_card/hello.cpp
@@ -0,0 +1,13 @@
+//Creating a student report card
+#include<iostream>
+using namespace std;
+int main(){
+    int studentID = 1001;
+    float score = 92.5;
+    char grade = 'A';
+
+    cout << studentID << "\n";
+    cout << score << "\n";
+    cout << grade << "\n";
+    return 0;
+}
