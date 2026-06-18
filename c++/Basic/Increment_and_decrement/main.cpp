d8dd07a867375a298615f994101ca5a3ee7f6cd5
chetanrathore423@gmail.com <chetanrathore423@gmail.com>
2026-06-19 04:27:54 +0530

    Add a new c++ program

---
+++ c++/Basic/Increment_and_decrement/main.cpp
@@ -0,0 +1,13 @@
+//Increament and decrement operators
+#include<iostream>
+using namespace std;
+int main(){
+    int x = 5;
+    ++x;
+    cout << x << endl;
+
+    int y = 7;
+    --y;
+    cout << y << endl;;
+    return 0;
+}
